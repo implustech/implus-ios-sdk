ffe700449a5da2ba17bb864bf930661fe72b47eb
Vilius <vilius.pad@gmail.com>
2020-10-22 16:01:52 +0300

    Apple Search Ads attributions

---
+++ Pods/Target Support Files/Founq_KeychainIDFA/Founq_KeychainIDFA-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "KeychainHelper.h"
+#import "KeychainIDFA.h"
+
+FOUNDATION_EXPORT double Founq_KeychainIDFAVersionNumber;
+FOUNDATION_EXPORT const unsigned char Founq_KeychainIDFAVersionString[];
+
